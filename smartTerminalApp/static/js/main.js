d68648c44b7721715bfa61929205da502b20250c
Liu Jun <jun0523@gmail.com>
2018-05-25 15:18:37 +0800

    no message

--- smartTerminalApp/static/js/main.js
+++ smartTerminalApp/static/js/main.js
@@ -1,39 +1,35 @@
 (function () {
-
     window.addEventListener('DOMContentLoaded', function () {
-        var isStreaming = false;
-        var start = document.getElementById('start');
+        var isStreaming = true;
+        var start = document.getElementById('btn_start');
         var video = document.getElementById('video');
         var canvas = document.getElementById('canvas');
         var ctx = canvas.getContext('2d');
+        var wsurl = 'ws://localhost:80/webrtc';
 
-        start.addEventListener('click', function (e) {
-            var wsurl = 'ws://localhost:80/webrtc';
-
-            if (!isStreaming) {
-                signal(wsurl,
-                        function (stream) {
-                            console.log('got a stream!');
-                            var url = window.URL || window.webkitURL;
-                            video.src = url ? url.createObjectURL(stream) : stream;
-                            video.play();
-                        },
-                        function (error) {
-                            alert(error);
-                        },
-                        function () {
-                            console.log('websocket closed. bye bye!');
-                            video.src = '';
-                        },
-                        function (message) {
-                            alert(message);
-                        }
-                );
-            }
-        }, false);
+        if (!isStreaming) {
+            signal(wsurl,
+                    function (stream) {
+                        console.log('got a stream!');
+                        var url = window.URL || window.webkitURL;
+                        video.src = url ? url.createObjectURL(stream) : stream;
+                        video.play();
+                    },
+                    function (error) {
+                        alert(error);
+                    },
+                    function () {
+                        console.log('websocket closed. bye bye!');
+                        video.src = '';
+                    },
+                    function (message) {
+                        alert(message);
+                    }
+            );
+        }
 
         // Wait until the video stream can play
-        video.addEventListener('canplay', function (e) {
+        video.addEventListener('canplay', function () {
             if (!isStreaming) {
                 isStreaming = true;
             }
@@ -41,7 +37,7 @@
 
         // Wait for the video to start to play
         video.addEventListener('play', function () {
-            // Every 33 milliseconds copy the video image to the canvas
+            // Every 10 milliseconds copy the video image to the canvas
             setInterval(function () {
                 if (video.paused || video.ended) {
                     return;
@@ -52,3 +48,62 @@
         }, false);
     });
 })();
+
+$(function() {
+    $("#btn_start").click(function() {
+        var t = 0;
+        var interval = $("#interval").val();
+        var time = parseInt($("#remainSeconds").html()) * 60;
+        var startTime = new Date().getTime();
+        // Changing image according interval time
+        var intervalFuc = setInterval(function() {
+                switch (t%2){
+                    case 0 : {
+                        t = 1;
+                        document.getElementById("image").src="/static/img/right.jpg";
+                        break;
+                    }
+                    case 1 : {
+                        t = 0;
+                        document.getElementById("image").src="/static/img/left.jpg";
+                        break;
+                    }
+                }
+
+                if(new Date().getTime() - startTime >= time * 1000) {
+                    clearInterval(intervalFuc);
+                    return;
+                }
+            }, interval * 1000);
+	});
+});
+
+$(document).ready(function () {
+    $("#btn_start").click(function() {
+        SysSecond = parseInt($("#remainSeconds").html()) * 60;
+        if (SysSecond != "" && !isNaN(SysSecond)) {
+            InterValObj = window.setInterval(SetRemainTime, 1000);
+        }
+    });
+});
+
+function SetRemainTime() {
+    if (SysSecond > 0) {
+        SysSecond = SysSecond - 1;
+        var second = padLeft(Math.floor(SysSecond % 60), 2);
+        var minite = padLeft(Math.floor((SysSecond / 60) % 60), 2);
+
+        $("#remainTime").html('<h2 class="text-left text-danger">Testing Time: ' + minite + ':' + second + '</h2>');
+    } else {
+        window.clearInterval(InterValObj);
+        // Upload
+        document.getElementById("").click();
+    }
+}
+
+function padLeft(str, lenght) {
+    if (str.toString().length < lenght)
+        return padLeft("0" + str, lenght);
+    else
+        return str;
+}
--- smartTerminalApp/views.py
+++ smartTerminalApp/views.py
@@ -10,12 +10,14 @@ def index(request):
     if request.method == 'POST':
         patient_id = request.POST.get('patientID')
         interval = request.POST.get('interval')
+        time = request.POST.get('time')
 
         patient_id = patient_id.strip()
         if patient_id == 'P1':
             context['patientID'] = patient_id
             context['interval'] = interval
-            return redirect('main', params=patient_id + ',' + interval)
+            context['time'] = time
+            return redirect('main', params=patient_id + ',' + interval + ',' + time)
         else:
             context['message'] = 'Patient ID does not exist!'
             template_name = 'index.html'
@@ -28,7 +30,18 @@ def main(request, params):
     if request.method == 'GET':
         patient_id = params.split(',')[0]
         interval = params.split(',')[1]
+        time = params.split(',')[2]
         context['patientID'] = patient_id
         context['interval'] = interval
+        context['time'] = time
+        if len(time) < 2:
+            show_time = '0' + time
+        else:
+            show_time = time
+
+        context['show_time'] = show_time + ':00'
+
+    if request.method == 'POST':
+        context['patientID'] = ""
 
     return render(request, 'main.html', context)
